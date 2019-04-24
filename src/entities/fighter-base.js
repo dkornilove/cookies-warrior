098ac9ba0ae3afc627b012336ce86bc98a27029d
Dmitry Kornilov <norz.qq@gmail.com>
2019-04-24 16:36:49 +0400

    track base class file

---
+++ src/entities/fighter-base.js
@@ -0,0 +1,41 @@
+export const prettifyValue = value => Number(value.toFixed(1));
+export const pickRndValueFromArray = arr => arr[Math.floor(Math.random() * arr.length)];
+
+export default class FIghterBase {
+  calcAttack(res, def = 0) {
+    const value = this.attack * (1 - res) - def;
+    return value < 0 ? 0 : prettifyValue(value);
+  }
+
+  getFinalModifier(res, def) {
+    return this.plans[this.plan].getModifier(res, def);
+  }
+
+  getStats() {
+    return {
+      hp: prettifyValue(this.hp),
+      def: prettifyValue(this.defense),
+      res: prettifyValue(this.resistance),
+      attack: prettifyValue(this.attack),
+    };
+  }
+
+  getPlan(res) {
+    return this.plans[this.plan].toString(res);
+  }
+
+  flash(method, attribute, value) {
+    if (!this[method]) {
+      this[method] = [];
+    }
+    this[attribute] += value;
+    this[method].push([attribute, -value]);
+  }
+
+  reset(method) {
+    this[method].forEach(([attribute, value]) => {
+      this[attribute] += value;
+    });
+    this[method] = [];
+  }
+}
