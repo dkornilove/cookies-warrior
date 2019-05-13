5d54e216e9e5b360379146b1f4cedcd85acb1871
Dmitry Kornilov <norz.qq@gmail.com>
2019-05-13 16:22:26 +0400

    refactor entities data

--- src/data/entities.js
+++ src/data/entities.js
@@ -1,29 +1,32 @@
-// [[name, description], [[method, target, attribute, value],[...]], rarity]
+// [name#meta#rarity, [method#target#attribute#value, ...]]
+
 const cookies = [
   ['Sugar cookie##1', ['boost#player#attack#+3']],
   ['Milk cookie##1', ['boost#player#defense#+3']],
-  ['Honey cookie#Consumable:#0.4', ['boost#player#attack#+5']],
-  ['Chocolate cookie##0.8', ['boost#player#attack#+3', 'boost#player#defense#+1']],
+  ['Honey cookie##0.8', ['boost#player#attack#+5']],
+  ['Ice cookie##0.8', ['boost#player#defense#+5']],
+  ['Chocolate cookie##0.8', ['boost#player#attack#+3, boost#player#defense#+2']],
   ['Charcoal biscuit#Consumable:#0.2', ['break#monster#hp#-3', 'break#player#hp#+3']],
-  /* [['Void cookie', '+2 attack until end of fight'], [['patch', 'player', 'attack', +2]], 0.6],
-  [['Lime cookie', '-3 monster attack until end of turn'], [['boost', 'monster', 'attack', -3]], 0.6],
-  [['Сracker', '+5 attack, +5 defense until end of turn'], [['boost', 'player', 'attack', +5], ['boost', 'player', 'defense', +5]], 0.3],
-  [['Biscuit', '+0.3 resistance until end of turn'], [['boost', 'player', 'resistance', +0.3]], 0.4],
-  [['Bourbon biscuit', '-1 attack, -1 defense, -0.1 resistance monster until end of fight'], [['patch', 'monster', 'attack', -1], ['patch', 'monster', 'defense', -1], ['patch', 'monster', 'resistance', -0.1]], 0.3],
-  [['Fortune cookie', '+1 attack until end of fight'], [['patch', 'player', 'attack', +1]], 0.4],
-  [['Cream cracker', '-2 attack monster until end of fight'], [['patch', 'monster', 'attack', -2]], 0.4],
-  [['Oreo', '+8 attack until end of turn'], [['boost', 'player', 'attack', +8]], 0.2],
-  [['Rosette', '+1 attack, +1 defense, +0.1 resistance until end of fight'], [['patch', 'player', 'attack', +1], ['patch', 'player', 'defense', +1], ['patch', 'player', 'resistance', +0.1]], 0.4],
-  [['Gingerbread man', '-3 hp monster'], [['break', 'monster', 'hp', -3]], 0.3],
-  [['Digestive biscuit', '+3 hp'], [['break', 'player', 'hp', +3]], 0.3],
-  [['Carrot cookie', '+1 cookie until end of fight'], [['patch', 'player', 'cookiesPerMove', +1], ['patch', 'player', 'getcookies', +1]], 0.3],
-  [['Butter cookie', '+5 attack, -1 cookie until end of fight'], [['patch', 'player', 'cookiesPerMove', -1], ['patch', 'player', 'getcookies', -1], ['patch', 'player', 'attack', +5]], 0.3], */
-
+  ['Void cookie#Consumable:#0.4', ['patch#player#attack#+2']],
+  ['Creamy cookie#Consumable:#0.4', ['patch#player#defense#+2']],
+  ['Lime cookie##0.8', ['boost#monster#attack#-3']],
+  ['Сracker##0.2', ['boost#player#attack#+5', 'boost#player#defense#+4']],
+  ['Biscuit##0.4', ['boost#player#resistance#0.3']],
+  ['Bourbon biscuit#Consumable:#0.4', ['patch#monster#attack#-1', 'patch#monster#defense#-1', 'patch#monster#resistance#-0.1']],
+  ['Fortune cookie##1', ['patch#player#attack#+1']],
+  ['Cream cracker##1', ['patch#monster#attack#-1']],
+  ['Oreo#Consumable:#0.2', ['boost#player#attack#+8']],
+  ['Choco-Pie#Consumable:#0.2', ['boost#player#defense#+8']],
+  ['Rosette#Consumable:#0.4', ['patch#player#attack#+1', 'patch#player#defense#+1', 'patch#player#resistance#+0.1']],
+  ['Gingerbread man##0.8', ['break#monster#hp#-3']],
+  ['Digestive biscuit##0.8', ['break#player#hp#+3']],
+  ['Carrot cookie#Consumable:#0.4', ['patch#player#cookiesPerMove#+1', 'patch#player#getcookies#+1']],
+  ['Butter cookie#Consumable:#0.2', ['patch#player#cookiesPerMove#-1', 'patch#player#getcookies#-1', 'patch#player#attack#+4']],
 ];
 
 // [name, hp, attack. defenseValue, resistance, rarity]
 const monsters = [
-  ['Dummy', 10, 6, 5, 0, 1],
+  ['Creepy Dog', 10, 6, 5, 0, 1],
   ['Pretty Demon', 11, 7, 6, 0, 1],
   ['Snowman John', 13, 5, 7, 0, 1],
   ['Ugly Fish', 14, 4, 8, 0, 1],
@@ -31,8 +34,8 @@ const monsters = [
   ['Crazy Octupus', 14, 5, 7, 0, 1],
   ['Horde Leader', 14, 4, 6, 0, 1],
   ['Strange Entity', 11, 7, 5, 0, 1],
-  ['Old Woman', 12, 6, 8, 0, 1],
-  ['Old Man', 15, 6, 8, 0, 0.9],
+  ['Walking dead', 12, 6, 8, 0, 1],
+  ['DeathEater', 15, 6, 8, 0, 0.9],
   ['Legion Commander', 16, 7, 9, 0.06, 0.9],
   ['Warlock', 17, 8, 8, 0.07, 0.8],
   ['Gremlin', 18, 9, 9, 0.08, 0.8],
@@ -43,51 +46,51 @@ const monsters = [
 
 ];
 
-// [[name, description], [[method, target, attribute, value],[...]], rarity]
+// [name#meta#rarity, [method#target#attribute#value, ...]]
 const spells = [
   ['Knock out the weapon##1', ['boost#player#attack#-10']],
   ['Snow defense$##1', ['patch#monster#defense#+2']],
-  /* [['Fury', '+2 monster attack until end of fight'], [['patch', 'monster', 'attack', +2]], 1],
-  [['Nature`s Call', '+0.1 monster resistance until end of fight'], [['patch', 'monster', 'resistance', +0.1]], 1],
-  [['Old Curse', '-2 player attack until end of fight'], [['patch', 'player', 'attack', -2]], 1],
-  [['Plague', '-0.2 player resistance until end of fight'], [['patch', 'player', 'resistance', -0.2]], 1],
-  [['Unholy Strange', '+4 monster attack until end of fight'], [['patch', 'monster', 'attack', +5]], 0.8],
-  [['Steal Cookie', '-1 player cookie until end of fight'], [['patch', 'player', 'cookiesPerMove', -1], ['patch', 'player', 'getcookies', -1]], 0.8], */
+  ['Fury##1', ['patch#monster#attack#+3']],
+  ['Nature`s Call##1', ['patch#monster#resistance#+0.1']],
+  ['Old Curse##1', ['patch#player#attack#-2']],
+  ['Plague##1', ['patch#player#resistance#-0.2']],
+  ['Unholy Strange##1', ['patch#monster#attack#+4']],
+  ['Steal Cookie##1', ['patch#player#cookiesPerMove#-1', 'patch#player#getcookies#-1']],
 
 ];
 
-// [[name, description], [[method, target, attribute, value],[...]], rarity]
+// [name#meta#rarity, [method#target#attribute#value, ...]]
 const environments = [
   ['Yellow Plane#Cold:#1', ['patch#player#attack#-1']],
-  ['Blue Swamp#Warm:#0.8', ['patch#monster#attack#+1', 'patch#player#attack#+1']],
-  /* [['Mountains', 'Fear: -1 cookie'], [['patch', 'player', 'cookiesPerMove', -1], ['patch', 'player', 'getcookies', -1]], 0.8],
-  [['Magic Forest', 'Joy: +1 defense player'], [['patch', 'player', 'defense', +1]], 1],
-  [['Abandoned Village', 'Sadness: +0.1 resistance monster'], [['patch', 'monster', 'resistance', +0.1]], 1],
-  [['Deep Cave', 'Dark: +2 attack monster, +0.1 resistance player'], [['patch', 'monster', 'attack', +2], ['patch', 'player', 'resistance', +0.1]], 1],
-  [['Quagmire', 'Stickiness: +1 player defense'], [['patch', 'player', 'deffense', +1]], 1],
-  [['Green Steppe', 'Freedom: +1 cookie'], [['patch', 'player', 'cookiesPerMove', +1], ['patch', 'player', 'getcookies', +1]], 0.7],
-  [['Cemetery', 'Panic: -0.1 resistance player, -1 attack player'], [['patch', 'player', 'resistance', -0.1], ['patch', 'player', 'attack', -1]], 1],
-  [['Derelict Camp', 'Sadness: +0.1 resistance monster'], [['patch', 'monster', 'resistance', +0.1]], 1],
-  [['Lawn with a Fire', 'Warm: +1 attack monster, +1 attack player'], [['patch', 'monster', 'attack', +1], ['patch', 'player', 'attack', +1]], 1],
-  [['Smelly Lane', 'Disgust: -1 attack player, +1 defense monster'], [['patch', 'monster', 'defense', +1], ['patch', 'player', 'attack', -1]], 1], */
+  ['Blue Swamp#Warm:#0.8', ['patch#monster#attack#+1#patch#player#attack#+1']],
+  ['Mountains#Fear:#0.8', ['patch#player#cookiesPerMove#-1', 'patch#player#getcookies#-1']],
+  ['Magic Forest#Joy:#1', ['patch#player#defense#+1']],
+  ['Abandoned Village#Sadness:#1', ['patch#monster#resistance#+0.1']],
+  ['Deep Cave#Dark:#1', ['patch#monster#attack#+2', 'patch#player#resistance#+0.1']],
+  ['Quagmire#Stickiness:#1', ['patch#player#deffense#+1']],
+  ['Green Steppe#Freedom:#0.4', ['patch#player#cookiesPerMove#+1', 'patch#player#getcookies#+1']],
+  ['Cemetery#Panic:#1', ['patch#player#resistance#-0.1', 'patch#player#attack#-1']],
+  ['Derelict Camp#Sadness:#1', ['patch#monster#resistance#+0.1']],
+  ['Lawn with a Fire#Warm:1', ['patch#monster#attack#+1', 'patch#player#attack#+1']],
+  ['Smelly Lane#Disgust:#1', ['patch#monster#defense#+1', 'patch#player#attack#-1']],
 
 ];
 
-// [[name, description], [[method, target, attribute, value],[...]], rarity]
+// [name#meta#rarity, [method#target#attribute#value, ...]]
 const artefacts = [
   ['Stick of the truth#Applies at the level start:#1', ['patch#player#attack#+1']],
   ['Ogre Belt#Applies at the level start:#1', ['patch#player#defense#+1']],
-  /* [['Horseshoe', '+1 cookie'], [['patch', 'player', 'cookiesPerMove', +1], ['patch', 'player', 'getcookies', +1]], 0.7],
-  [['Staff of Humiliation', '-0.1 monster resistance'], [['patch', 'monster', 'resistance', -0.1]], 1],
-  [['Sword of Justice', '+1 player attack, -0.1 monster resistance'], [['patch', 'player', 'attack', +1], ['patch', 'monster', 'resistance', -0.1]], 0.95],
-  [['Rusty Blade', '-1 monster attack'], [['patch', 'monster', 'attack', -1]], 1],
-  [['Worn Shield', '+1 player defense, +0.1 player resistance'], [['patch', 'player', 'defense', +1], ['patch', 'player', 'resistance', +0.1]], 0.9],
-  [['Bottled Poison', '-0.1 monster resitance, -1 monster attack, -1 monster defense'], [['patch', 'monster', 'resistance', -0.1], ['patch', 'monster', 'attack', -1], ['patch', 'monster', 'defense', -1]], 0.7],
-  [['Mercy Hat', '+2 player defense'], [['patch', 'player', 'defense', +2]], 1],
-  [['InsideOut Shirt', '-0.1 player resistance, +2 player attack'], [['patch', 'player', 'attack', +2], ['patch', 'player', 'resistance', -0.1]], 1],
-  [['Mail of Fidelity', '+0.1 player resistance'], [['patch', 'player', 'resitance', +0.1]], 1],
-  [['Dragon Eye', '+0.1 player resistance, +1 player attack, +1 player defense'], [['patch', 'player', 'defense', +1], ['patch', 'player', 'attack', +1], ['patch', 'player', 'resitance', +0.1]], 0.8],
-  [['Gobelet of Fire', '+1 player attack, +2 player defense'], [['patch', 'player', 'attack', +1], ['patch', 'player', 'defense', +2]], 0.8], */
+  ['Horseshoe#Applies at the level start:#0.8', ['patch#player#cookiesPerMove#+1', 'patch#player#getcookies#+1']],
+  ['Staff of Humiliation#Applies at the level start:#1', ['patch#monster#resistance#-0.1']],
+  ['Sword of Justice#Applies at the level start:#0.8', ['patch#player#attack#+1', 'patch#monster#resistance#-0.1']],
+  ['Rusty Blade#Applies at the level start:#1', ['patch#monster#attack#-1']],
+  ['Worn Shield#Applies at the level start:#0.8', ['patch#player#defense#+1', 'patch#player#resistance#+0.1']],
+  ['Bottled Poison#Applies at the level start:#0.4', ['patch#monster#resistance#-0.1', 'patch#monster#attack#-1', 'patch#monster#defense#-1']],
+  ['Mercy Hat#Applies at the level start:#1', ['patch#player#defense#+2']],
+  ['InsideOut Shirt#Applies at the level start:#1', ['patch#player#attack#+2', 'patch#player#resistance#-0.1']],
+  ['Mail of Fidelity#Applies at the level start:#1', ['patch#player#resitance#+0.1']],
+  ['Dragon Eye#Applies at the level start:#0.8', ['patch#player#defense#+1', 'patch#player#attack#+1', 'patch#player#resitance#+0.1']],
+  ['Gobelet of Fire#Applies at the level start:#0.8', ['patch#player#attack#+1', 'patch#player#defense#+2']],
 
 ];
 
